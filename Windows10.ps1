bff8c507a098d4e74216acb20d3c6f020e09a875
cdgstephan <101641249+cdgstephan@users.noreply.github.com>
2022-03-26 12:30:30 +0100

    Update and rename Win1021H2.ps1 to Windows10.ps1

--- Windows10.ps1
+++ Windows10.ps1
@@ -1,21 +1,19 @@
 Write-Host  -ForegroundColor Cyan "Starting Circet OSD 21H2 with Firmware Update"
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 3
 
-#Change Display Resolution for Virtual Machine
+#Change Display Resolution for Lenovo T440s
 Write-Host  -ForegroundColor Cyan "Setting Display Resolution for Lenovo T440s"
-if ((Get-MyComputerProduct) -match '') {
+if ((Get-MyComputerProduct) -match '20AR') {
     Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1920x"
     Set-DisRes 1920
 }
 
-#Make sure I have the latest OSD Content
+#Make sure we have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
 Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
-
-#Start-Sleep -Seconds 5
 
 #Start OSDCloud ZTI 
 Write-Host  -ForegroundColor Cyan "Start OSD Windows 10 21H2 with Firmware Update"
